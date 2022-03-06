64b3db0d2e6ddce1f8cda20f49e846f151058b39
Tejas-coder <tejaskaranjvkar@gmail.com>
2022-03-06 21:07:48 +0530

    initial commit

---
+++ src/data6.js
@@ -0,0 +1,96 @@
+import IMG1 from "./image/bird1.jpg";
+import IMG2 from "./image/bird2.jpg";
+import IMG3 from "./image/bird3.jpg";
+import IMG4 from "./image/bird4.jpg";
+import IMG5 from "./image/bird5.jpg";
+import IMG6 from "./image/bird6.jpg";
+import IMG7 from "./image/bird7.png";
+import IMG8 from "./image/bird8.jpg";
+import IMG9 from "./image/bird9.jpg";
+import IMG10 from "./image/dog1.jpeg";
+import IMG11 from "./image/dog2.jpeg";
+import IMG12 from "./image/dog3.jpeg";
+import IMG13 from "./image/dog4.jpeg";
+import IMG14 from "./image/dog5.jpeg";
+import IMG15 from "./image/dog6.jpeg";
+import IMG16 from "./image/dog7.jpeg";
+import IMG17 from "./image/dog8.jpeg";
+import IMG18 from "./image/dog9.jpeg";
+
+
+const uniqueCardsArray = [
+  {
+    type: "bird3",
+    image: IMG1,
+  },
+  {
+    type: "bird5",
+    image: IMG2,
+  },
+  {
+    type: "bird6",
+    image: IMG3,
+  },
+  {
+    type: "bird7",
+    image: IMG4,
+  },
+  {
+    type: "bird8",
+    image: IMG5,
+  },
+  {
+    type: "bird2",
+    image: IMG6,
+  },
+  {
+    type: "bird4",
+    image: IMG7,
+  },
+  {
+    type: "bird9",
+    image: IMG8,
+  },
+  {
+    type: "bird1",
+    image: IMG9,
+  },
+  {
+    type: "dog1",
+    image: IMG10
+  },
+  {
+    type: "dog2",
+    image: IMG11
+  },
+  {
+    type: "dog3",
+    image: IMG12
+  },
+  {
+    type: "dog4",
+    image: IMG13
+  },
+  {
+    type: "dog5",
+    image: IMG14
+  },
+  {
+    type: "dog6",
+    image: IMG15
+  },
+  {
+    type: "dog7",
+    image: IMG16
+  },
+  {
+    type: "dog8",
+    image: IMG17
+  },
+  {
+    type: "dog8",
+    image: IMG18
+  }
+];
+
+export default uniqueCardsArray;
---
+++ src/pages/homePage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Complexcity6 from "../../components/complexsity6"
+
+function HomePage() {
+  return (
+    <Complexcity6 />
+  )
+}
+
+export default HomePage
